71a8e62155d6969cdc972d10f1bddd193facd027
chenzeshenga <chenzeshenga@163.com>
2018-02-28 00:02:06 +0800

    add loader to load data

---
+++ bondstatic/src/main/java/com/chenzeshenga/loader/GovernmentBondShanghaiLoader.java
@@ -0,0 +1,29 @@
+package com.chenzeshenga.loader;
+
+import java.util.Date;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import redis.clients.jedis.Jedis;
+
+@Component
+public class GovernmentBondShanghaiLoader {
+
+	@Autowired
+	private RestTemplate restTemplate;
+
+	@Autowired
+	private Jedis jedis;
+
+	@PostConstruct
+	public void init() {
+		String str = restTemplate.getForObject(
+				"http://nufm.dfcfw.com/EM_Finance2014NumericApplication/JS.aspx?type=CT&cmd=C._DEBT_SH_G&sty=FCOIATA&sortType=(ChangePercent)&sortRule=-1&page=2&pageSize=20&token=7bc05d0d4c3c22ef9fca8c2a912d779c&jsName=quote_123&_g=0.628606915911589&_=1519746540141",
+				String.class);
+		jedis.set(new Date().toString(), str);
+	}
+}
---
+++ bondstatic/src/main/java/com/chenzeshenga/loader/TotalIndexLoader.java
@@ -0,0 +1,29 @@
+package com.chenzeshenga.loader;
+
+import java.util.Date;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import redis.clients.jedis.Jedis;
+
+@Component
+public class TotalIndexLoader {
+
+	@Autowired
+	private RestTemplate restTemplate;
+
+	@Autowired
+	private Jedis jedis;
+
+	@PostConstruct
+	public void init() {
+		String str=restTemplate.getForObject(
+				"http://nufm.dfcfw.com/EM_Finance2014NumericApplication/JS.aspx?type=CT&cmd=3994812,3950322,3950312,3950222,3950212,0000611,0000221,0000131,0000121&sty=MPICTTA&sortType=(ChangePercent)&sortRule=-1&page=1&pageSize=20&token=7bc05d0d4c3c22ef9fca8c2a912d779c&jsName=quote_123&_g=0.628606915911589&_=1519745135464",
+				String.class);
+		jedis.set(new Date().toString(), str);
+	}
+}
