2c5bd8ed0415c7d98d9827ba198d6b8b39920b5d
chenzeshenga <chenzeshenga@163.com>
2018-02-28 23:19:36 +0800

    add log

--- bondstatic/src/main/java/com/chenzeshenga/loader/CorpBondShanghaiLoader.java
+++ bondstatic/src/main/java/com/chenzeshenga/loader/CorpBondShanghaiLoader.java
@@ -6,6 +6,7 @@ import java.util.Map;
 
 import javax.annotation.PostConstruct;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -15,6 +16,8 @@ import redis.clients.jedis.Jedis;
 
 @Component
 public class CorpBondShanghaiLoader {
+
+	private static Logger logger = Logger.getLogger(CorpBondShanghaiLoader.class);
 
 	@Autowired
 	private RestTemplate restTemplate;
@@ -27,12 +30,12 @@ public class CorpBondShanghaiLoader {
 	public void init() {
 		String restAPI = "http://nufm.dfcfw.com/EM_Finance2014NumericApplication/JS.aspx?type=CT&cmd=C._DEBT_SH_Q&sty=FCOIATA&sortType=(ChangePercent)&sortRule=-1&page=1&pageSize=20&token=7bc05d0d4c3c22ef9fca8c2a912d779c&jsName=quote_123&_g=0.628606915911589&_=1519830348062";
 		for (int i = 0; i < 100; i++) {
-			String str = restTemplate.getForObject(restAPI.replace("page=1", "page=" + (i+1)), String.class);
+			String str = restTemplate.getForObject(restAPI.replace("page=1", "page=" + (i + 1)), String.class);
 			String reg1 = "\"" + "," + "\"";
 			String rep1 = "\"" + ";" + "\"";
 			str = str.replaceAll("\\(\\[", "").replaceAll("\\]\\)", "").replaceAll(reg1, rep1);
 			String[] strs = str.split(";");
-			
+
 			for (int j = 0; j < strs.length; j++) {
 				String[] childs = strs[j].replaceAll("\"", "").split(",");
 				Map<String, String> map = new HashMap<String, String>();
@@ -50,6 +53,8 @@ public class CorpBondShanghaiLoader {
 				map.put("dealquantity", childs[12]);
 				map.put("timestamp", new Date().toString());
 				jedis.hmset(childs[1], map);
+				logger.info(childs[1]);
+				logger.info(map);
 			}
 			try {
 				Thread.sleep(1000);
