e8613f636d8380ae9f5d95917c0c675cc7a54279
chenzeshenga <chenzeshenga@163.com>
2018-03-05 23:21:37 +0800

    add controller for API

--- bondstatic/src/main/java/com/chenzeshenga/controller/BondStaticController.java
+++ bondstatic/src/main/java/com/chenzeshenga/controller/BondStaticController.java
@@ -1,8 +1,13 @@
 package com.chenzeshenga.controller;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import redis.clients.jedis.Jedis;
@@ -14,11 +19,107 @@ public class BondStaticController {
 	@Autowired
 	private Jedis jedis;
 
-	@RequestMapping("/index")
-	public String getIndex(@RequestParam String indexcode, @RequestParam String days) {
+	@RequestMapping("/hisindex")
+	@ResponseBody
+	public String getHisIndex(@RequestParam String indexcode, @RequestParam String days) {
 		System.err.println("indexcode=" + indexcode + ",days=" + days);
-		String result = jedis.get(indexcode);
-		return result;
+		Iterator<String> iter = jedis.hkeys(indexcode).iterator();
+		List<String> data = new ArrayList<String>();
+
+		while (iter.hasNext()) {
+			String key = iter.next();
+			List<String> value = jedis.hmget(indexcode, key);
+			System.out.println(key + ":" + value.get(0));
+			data.add(value.get(0));
+		}
+
+		StringBuilder sb = new StringBuilder();
+
+		for (int i = 0; i < data.size(); i++) {
+			sb.append(",");
+			sb.append(data.get(i));
+		}
+
+		System.err.println(sb);
+		return sb.toString().replaceFirst(",", "");
+
+	}
+
+	@RequestMapping("/index")
+	@ResponseBody
+	public String getIndex(@RequestParam String indexcode) {
+		System.err.println("indexcode=" + indexcode);
+		Iterator<String> iter = jedis.hkeys(indexcode).iterator();
+		List<String> data = new ArrayList<String>();
+
+		while (iter.hasNext()) {
+			String key = iter.next();
+			List<String> value = jedis.hmget(indexcode, key);
+			System.out.println(key + ":" + value.get(0));
+			data.add(value.get(0));
+		}
+
+		StringBuilder sb = new StringBuilder();
+
+		for (int i = 0; i < data.size(); i++) {
+			sb.append(",");
+			sb.append(data.get(i));
+		}
+
+		System.err.println(sb);
+		return sb.toString().replaceFirst(",", "");
+
+	}
+
+	@RequestMapping("/bond")
+	@ResponseBody
+	public String getBond(@RequestParam String indexcode) {
+		System.err.println("indexcode=" + indexcode);
+		Iterator<String> iter = jedis.hkeys(indexcode).iterator();
+		List<String> data = new ArrayList<String>();
+
+		while (iter.hasNext()) {
+			String key = iter.next();
+			List<String> value = jedis.hmget(indexcode, key);
+			System.out.println(key + ":" + value.get(0));
+			data.add(value.get(0));
+		}
+
+		StringBuilder sb = new StringBuilder();
+
+		for (int i = 0; i < data.size(); i++) {
+			sb.append(",");
+			sb.append(data.get(i));
+		}
+
+		System.err.println(sb);
+		return sb.toString().replaceFirst(",", "");
+
+	}
+
+	@RequestMapping("/hisBond")
+	@ResponseBody
+	public String getHisBond(@RequestParam String indexcode, @RequestParam String days) {
+		System.err.println("indexcode=" + indexcode + ",days=" + days);
+		Iterator<String> iter = jedis.hkeys(indexcode).iterator();
+		List<String> data = new ArrayList<String>();
+
+		while (iter.hasNext()) {
+			String key = iter.next();
+			List<String> value = jedis.hmget(indexcode, key);
+			System.out.println(key + ":" + value.get(0));
+			data.add(value.get(0));
+		}
+
+		StringBuilder sb = new StringBuilder();
+
+		for (int i = 0; i < data.size(); i++) {
+			sb.append(",");
+			sb.append(data.get(i));
+		}
+
+		System.err.println(sb);
+		return sb.toString().replaceFirst(",", "");
 
 	}
 }
